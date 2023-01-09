68e1aa5c27fcdd3645d720c803f518344fbfffde
fengyongzhen <yzfeng@cn.dten.com>
2023-01-09 16:52:39 +0800

    update BitStore

--- src/main/java/com/erma/util/bit/BitStore.java
+++ src/main/java/com/erma/util/bit/BitStore.java
@@ -10,7 +10,7 @@ public class BitStore {
     private final long[] value;
 
     public BitStore(long length) {
-        long size = length % 64 == 0 ? length >> 6 : length >> 6 + 1;
+        long size = length % 64 == 0 ? length >> 6 : (length >> 6) + 1;
         if (size > Integer.MAX_VALUE) {
             throw new IllegalArgumentException("The length must be less than " + Integer.MAX_VALUE * 64L);
         }
@@ -19,56 +19,38 @@ public class BitStore {
     }
 
     public byte get(long index) {
-        Position position = getPosition(index);
-        return getBit(value[position.getSeq()], position.getNewIndex());
+        long[] position = getPosition(index);
+        return getBit(value[(int) position[0]], position[1]);
     }
 
-    private Position getPosition(long index) {
+    private long[] getPosition(long index) {
         if (index > length) {
             throw new IllegalArgumentException("The index cannot be greater than " + length);
         }
-        return new Position((int) (index >> 6L), index % 64);
+        return new long[]{index >> 6, index % 64};
     }
 
     public void setOne(long index) {
-        Position position = getPosition(index);
-        value[position.getSeq()] = setBitToOne(value[position.getSeq()], position.getNewIndex());
+        long[] position = getPosition(index);
+        value[(int) position[0]] = setBitToOne(value[(int) position[0]], position[1]);
     }
 
     public void setZero(long index) {
-        Position position = getPosition(index);
-        value[position.getSeq()] = setBitToZero(value[position.getSeq()], position.getNewIndex());
+        long[] position = getPosition(index);
+        value[(int) position[0]] = setBitToZero(value[(int) position[0]], position[1]);
     }
 
     private byte getBit(long n, long m) {
-        return (byte) ((n >> (m - 1L)) & 1L);
+        return (byte) ((n >> (m - 1)) & 1);
     }
 
     private long setBitToOne(long n, long m) {
         //将1左移m-1位找到第m位，得到000...1...000n在和这个数做或运算
-        return n | (1L << (m - 1L));
+        return n | (1L << (m - 1));
     }
 
     private long setBitToZero(long n, long m) {
         //将1左移m-1位找到第m位，取反后变成111...0...1111n再和这个数做与运算
-        return n & ~(1L << (m - 1L));
-    }
-
-    private class Position {
-        private int seq;
-        private long newIndex;
-
-        public Position(int seq, long newIndex) {
-            this.seq = seq;
-            this.newIndex = newIndex;
-        }
-
-        public int getSeq() {
-            return seq;
-        }
-
-        public long getNewIndex() {
-            return newIndex;
-        }
+        return n & ~(1L << (m - 1));
     }
 }
